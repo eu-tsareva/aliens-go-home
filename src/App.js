8215225f3084a97b706590e2b2b79648e7d66b8c
tsareva <e.tsareva@uit.space>
2018-03-10 14:49:09 +0300

    add redux and test data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Hello</h1>
+          <h1 className="App-title">{ this.props.message }</h1>
         </header>
       </div>
     );
   }
+}
+
+App.propTypes = {
+  message: PropTypes.string.isRequired,
 }
 
 export default App;
