8fd295923b432b77a8a58a71540f1f4327d55ed8
tsareva <e.tsareva@uit.space>
2018-03-16 21:32:46 +0300

    add static components, tracking action

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,20 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Sky from './Sky';
 import Ground from './Ground';
+import CannonBase from './CannonBase';
+import CannonPipe from './CannonPipe';
+import CannonBall from './CannonBall';
+import CurrentScore from './CurrentScore';
+import FlyingObject from './FlyingObject';
+import Heart from './Heart';
+import StartGame from './StartGame';
+import Title from './Title';
 
-const Canvas = () => {
+const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
   return (
     <svg
       id="aliens-go-home-canvas"
       preserveAspectRatio="xMaxYMax"
       viewBox={viewBox}
+      onMouseMove={props.trackMouse}
     >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
       <Sky />
       <Ground />
-      <circle cx={0} cy={0} r={50} />
+      <CannonPipe rotation={props.angle} />
+      <CannonBase />
+      <CannonBall position={ {x: 100, y: -100} }/>
+      <CurrentScore score={23} />
+      <FlyingObject position={{x: -150, y: -300}}/>
+      <FlyingObject position={{x: 150, y: -300}}/>
+      <Heart position={{x: -300, y: 35}} />
+      <StartGame onClick={() => console.log('asdf')} />
+      <Title />
     </svg>
   );
 };
+
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  trackMouse: PropTypes.func.isRequired,
+}
 
 export default Canvas;
